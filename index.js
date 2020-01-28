4e74617e9a3aafa2c34adc7db8b083d339d3d896
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-01-28 02:06:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,10 +20,11 @@ var animal = yourAnimal();
 
 
 function add2(n) {
-  return n + two
+  const two = 2;
+  return n + two;
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() {
