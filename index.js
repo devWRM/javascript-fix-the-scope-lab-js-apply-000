3086dc985a88148fd614772f784f780d6596cff9
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-01-28 01:19:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 var animal = 'dog'
 
-function myAnimal() {
+function myAnimal(dog) {
   return animal;
 }
 
-function yourAnimal(animal) {
+function yourAnimal(cat) {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
 
-  animal = cat;
   return animal;
 }
 
